c19701493bc51eb0d25a8e80f9507d7d856d8f01
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-04-19 05:55:27 +0530

    Step-2: Insert Data.sql
    Today Topic is Insert Data: Insert Into is used to add data into table.

---
+++ DAY 1 – SQL FOUNDATION.sql/Step-2: Insert Data.sql
@@ -0,0 +1,5 @@
+--Insert Data
+INSERT INTO Employees(ID, Name, Salary) VALUES(1, 'Raji', 50000.00);
+INSERT INTO Employees(ID, Name, Salary) VALUES(2, 'Chandra', 60000.50);
+INSERT INTO Employees(ID, Name, Salary) VALUES(3, 'Dheshna', 80000.75);
+INSERT INTO Employees(ID, Name, Salary) VALUES(4, 'Gagan', 90000.00);
