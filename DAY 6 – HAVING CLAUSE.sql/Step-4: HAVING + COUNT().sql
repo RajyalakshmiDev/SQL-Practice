180620d5e6339144026d213934f340d82f3de251
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-08 20:57:25 +0530

    Step-4: HAVING + COUNT().sql

---
+++ DAY 6 – HAVING CLAUSE.sql/Step-4: HAVING + COUNT().sql
@@ -0,0 +1,28 @@
+--Program: Display departments where more than 2 employees
+
+--Create Table
+CREATE TABLE employee_data (
+    id INT,
+    employee_name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT
+);
+
+--Insert Data
+INSERT INTO employee_data VALUES (1, 'Raji', 'IT', 50000);
+INSERT INTO employee_data VALUES (2, 'Ravi', 'HR', 40000);
+INSERT INTO employee_data VALUES (3, 'Sita', 'IT', 60000);
+INSERT INTO employee_data VALUES (4, 'Kiran', 'Sales', 45000);
+INSERT INTO employee_data VALUES (5, 'Arun', 'HR', 42000);
+INSERT INTO employee_data VALUES (6, 'Meena', 'IT', 55000);
+
+--Fetch Data
+SELECT department, COUNT(*) AS total_employees
+FROM employee_data
+GROUP BY department
+HAVING COUNT(*) > 2;
+
+--Output
+department	total_employees
+   IT	          3
+
