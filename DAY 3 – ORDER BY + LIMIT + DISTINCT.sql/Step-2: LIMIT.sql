d36ebf4b8236d8d847b1a81dd31128eb1e3449cd
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-08 14:44:33 +0530

    Step-2: LIMIT.sql
    LIMIT restricts number of rows.

--- DAY 3 – ORDER BY + LIMIT + DISTINCT.sql/Step-2: LIMIT.sql
+++ DAY 3 – ORDER BY + LIMIT + DISTINCT.sql/Step-2: LIMIT.sql
@@ -1,1 +1,22 @@
+-- Create table
+CREATE TABLE employees (
+    id INT,
+    name VARCHAR(50),
+    salary INT
+);
 
+-- Insert Data
+INSERT INTO employees VALUES (1, 'Asha', 50000);
+INSERT INTO employees VALUES (2, 'Ravi', 40000);
+INSERT INTO employees VALUES (3, 'Kiran', 60000);
+INSERT INTO employees VALUES (4, 'Meena', 45000);
+
+--Fetch Data
+SELECT *
+FROM employees
+LIMIT 2;
+
+--Output:
+id	name	salary
+1	Asha	50000
+2	Ravi	40000
