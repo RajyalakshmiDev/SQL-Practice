d6606e7d4ce299f862b5b563abb278013d0ddd24
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-08 14:48:44 +0530

    Step-3: DISTINCT.sql
    DISTINCT removes duplicate values.

--- DAY 3 – ORDER BY + LIMIT + DISTINCT.sql/Step-3: DISTINCT.sql
+++ DAY 3 – ORDER BY + LIMIT + DISTINCT.sql/Step-3: DISTINCT.sql
@@ -1,1 +1,21 @@
+-- Create table
+CREATE TABLE products (
+    id INT,
+    category VARCHAR(50)
+);
 
+-- Insert Data
+INSERT INTO products VALUES (1, 'Electronics');
+INSERT INTO products VALUES (2, 'Clothes');
+INSERT INTO products VALUES (3, 'Electronics');
+INSERT INTO products VALUES (4, 'Shoes');
+
+--Fetch Data
+SELECT DISTINCT category
+FROM products;
+
+--Output:
+category
+Electronics
+Clothes
+Shoes
