95f426971c6f224966307591f5347a9346337e2c
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-08 15:13:37 +0530

    Step-1: Types of Aggregate Functions.sql
    Many rows → One result = Aggregate Function

---
+++ DAY 4 – AGGREGATE FUNCTIONS/Step-1: Types of Aggregate Functions.sql
@@ -0,0 +1,5 @@
+----Definition:
+Aggregate Functions are used to perform calculations on multiple rows and return a single result.
+  
+  There are 5 types of Aggregate functions:
+1 COUNT()  2️ SUM()  3️ AVG()  4️ MAX()  5️ MIN()
