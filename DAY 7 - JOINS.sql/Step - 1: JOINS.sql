af0da54b5529362395e8b04092dd5df71c360848
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-20 11:29:20 +0530

    Step - 1: JOINS.sql
    JOIN = Connect Tables

---
+++ DAY 7 - JOINS.sql/Step - 1: JOINS.sql
@@ -0,0 +1,13 @@
+--- Def: JOIN is used to combine data from multiple tables based on a related column.
+
+Example: 
+Table 1 → Employees
+emp_id	name	dept_id
+Table 2 → Departments
+dept_id	department_name
+Then We need to know which department an employee is in so we can join them.
+
+Types of JOINS: 1. INNER JOIN
+                2. LEFT JOIN
+                3. Right JOIN
+                4. FULL JOIN
