35586419205d089aee372df2b234dd18dfa0e690
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-05-25 15:32:35 +0530

    Create Step-3: Project.sql

---
+++ DAY 7 - JOINS.sql/Step-3: Project.sql
@@ -0,0 +1,19 @@
+Program:
+Step 1:
+CREATE TABLE employees(
+  emp_id INT,
+  name VARCHAR(50),
+  dept_id INT
+  );
+ INSERT INTO employees VALUES (1, 'Raji', 101);
+ INSERT INTO employees VALUES (2, 'Rama', 102);
+ INSERT INTO employees VALUES (3, 'Ritu', 103);
+ INSERT INTO employees VALUES (4, 'Rasi', 101);
+ 
+ CREATE TABLE departments(
+   dept_id INT,
+   department_name VARCHAR(50)
+   );
+ INSERT INTO departments VALUES (101, 'IT');
+ INSERT INTO departments VALUES (102, 'HR');
+ INSERT INTO departments VALUES (104, 'Sales');
