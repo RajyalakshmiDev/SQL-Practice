3c7a3cb1dd41ac5ff19a54715eabebae290d5ebe
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-06-08 18:26:39 +0530

    Step-4: Project.sql
    Select Employee details

--- DAY 1 – SQL FOUNDATION.sql/Step-4: Project.sql
+++ DAY 1 – SQL FOUNDATION.sql/Step-4: Project.sql
@@ -5,7 +5,7 @@ CREATE TABLE Employees (
   Salary DECIMAL(10, 2)
   );
 
--- Inser Data
+-- Insert Data
 INSERT INTO Employees(ID, Name, Salary) VALUES(1, 'Raji', 50000.00);
 INSERT INTO Employees(ID, Name, Salary) VALUES(2, 'Chandra', 60000.50);
 INSERT INTO Employees(ID, Name, Salary) VALUES(3, 'Dheshna', 80000.75);
