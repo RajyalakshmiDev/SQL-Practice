c47f15bbd314d8247dff4ddb2fedeb8b897c67cf
RajyalakshmiDev <rajyalakshmikaipu2424@gmail.com>
2026-07-01 23:59:00 +0530

    Step - 4: Project.sql

---
+++ DAY 8 – LEFT JOIN & RIGHT JOIN.sql/Step - 4: Project.sql
@@ -0,0 +1,21 @@
+Program:
+-----------
+Employee Department report using LEFT JOIN
+
+Description:
+------------
+A company stores employee information in one table and department information in another table.
+The goal of this project is to display all employees along with their department names. 
+If an employee is not assigned to any department, the report should display "No Department" instead of NULL.
+
+Requirement:
+-------------
+The report should display:
+Employee Name
+Department Name
+
+Conditions:
+Show all employees
+Display department name if available
+If department is not available, display "No Department"
+
